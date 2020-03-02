7038afbd33d7acabb39a215c6f0d807b122d2c7d
Mustafa Dalloul <mustafadall@mellanox.com>
2020-03-02 09:08:40 +0000

    [mstlink] Adding some fixes and updates
    Fixes and Changes:
    - Adding support to use PAM4 RX/TX rates(50G_1X, 100G_2X) in PRBS mode
    - Adding support to work with PAM4/NRZ speeds for new devices
    - Fixing JSON format issues
    - Updating PAOS failed message
    - Fixing ambiguous else if statements
    - Fixing split issues for Spectrum switch
    - Updating some of help messages
    - Validating the Firmware version while working on PCIe port
    - Adding support to access the PCIe port by "--port" flag
    - Adding RX and TX modulation for HDR speeds
    - Fixing lane width for 50G PAM4 speed
    
    Signed-off-by: Mustafa Dalloul <mustafadall@mellanox.com>

--- mlxlink/modules/printutil/mlxlink_record.h
+++ mlxlink/modules/printutil/mlxlink_record.h
@@ -285,7 +285,7 @@ public:
     static void printCmdLine(const std::string &line, Json::Value &jsonRoot);
     static void printErr(const std::string &err);
     static void printWar(const std::string &war, Json::Value &jsonRoot);
-    static std::vector<std::string> split(const std::string& str, char delim = ' ');
+    static std::vector<std::string> split(const std::string& str, const std::string &delim = " ");
     static void trim(std::string& str, const std::string& chars = "\t\n\v\f\r ");
 };
 
