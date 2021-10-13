81ddc3a98741727f8a6b7f289f343e1d35afae60
oshri <oshrid@nvidia.com>
2021-10-13 17:05:37 +0300

    Ported code changes from MFT 4.17.0, Spectrum2.csv is missing from mstdump
    Description:Align adabe_parser, add missing file

---
+++ adb_parser/adb_exceptionHolder.h
@@ -0,0 +1,114 @@
+/* 
+ * Copyright (C) Jan 2019 Mellanox Technologies Ltd. All rights reserved.
+ * 
+ * This software is available to you under a choice of one of two 
+ * licenses.  You may choose to be licensed under the terms of the GNU 
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the 
+ * OpenIB.org BSD license below:
+ * 
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ * 
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ * 
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+
+ *
+ */
+
+/*************************** ExceptionHolder ***************************/
+
+#ifndef ADB_EXCEPTIONHOLDER_H
+#define ADB_EXCEPTIONHOLDER_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include <cstdarg>
+#include <stdio.h>
+#include <stdarg.h>
+
+using namespace std;
+
+
+typedef vector<string> StringVector;
+typedef map<string, StringVector> ExceptionsMap;
+
+
+
+class ExceptionHolder {
+public:
+    // METHODS
+    static void insertNewException(const string exceptionType, string exceptionTxt);
+    static ExceptionsMap getAdbExceptionsMap();
+    static int getNumberOfExceptions();
+public:
+    // VARIABLES
+    static ExceptionsMap adbExceptionMap;
+    static const string FATAL_EXCEPTION;
+    static const string ERROR_EXCEPTION;
+    static const string WARN_EXCEPTION;
+    static int exceptionCounter;
+};
+
+/*************************** AdbException ***************************/
+class AdbException: public std::exception {
+public:
+    // Methods
+    AdbException();
+    AdbException(const char *msg, ...) __attribute__((format(__printf__, 2, 3)));
+    AdbException(string msg);
+    virtual ~AdbException() throw (); 
+    virtual const char* what() const throw (); 
+    virtual string what_s() const;
+
+private:
+    string _msg;
+};
+
+
+/**
+ * Function: ExceptionHolder::getNumberOfExceptions
+ * This function return the number of exceptions found
+ **/
+
+/*int ExceptionHolder::getNumberOfExceptions() {
+    return ExceptionHolder::exceptionCounter;
+}*/
+
+/**
+ * Function: ExceptionHolder::getAdbExceptionsMap
+ * This function return the adb exception map
+ **/
+
+/*ExceptionsMap ExceptionHolder::getAdbExceptionsMap() {
+    return ExceptionHolder::adbExceptionMap;
+}*/
+
+/**
+ * Function: ExceptionHolder::insertNewException
+ * This function take the excpetion type [FATAL:0, ERROR:1, WARNING:2] and the exception string
+ * Then it insert it to the adb exception map
+ **/
+/*void ExceptionHolder::insertNewException(const string exceptionType, string exceptionTxt) {
+    ExceptionHolder::adbExceptionMap[exceptionType].push_back(exceptionTxt);
+    ExceptionHolder::exceptionCounter += 1;
+}*/
+
+#endif
--- adb_parser/adb_xmlCreator.h
+++ adb_parser/adb_xmlCreator.h
@@ -1,11 +1,10 @@
-
 /* 
  * Copyright (C) Jan 2019 Mellanox Technologies Ltd. All rights reserved.
  * 
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
+ * This software is available to you under a choice of one of two 
+ * licenses.  You may choose to be licensed under the terms of the GNU 
  * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
+ * COPYING in the main directory of this source tree, or the 
  * OpenIB.org BSD license below:
  * 
  *     Redistribution and use in source and binary forms, with or
@@ -23,39 +22,42 @@
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
  * SOFTWARE.
 
  *
  */
 
-#ifndef ADB_EXPR_H
-#define ADB_EXPR_H
+#ifndef xmlCreator_H
+#define xmlCreator_H
 
-#include <map>
 #include <string>
-#include "expr.h"
-#include "adb_exceptionHolder.h"
+#include <common/compatibility.h>
 
 using namespace std;
 
-class AdbExpr : public Expr
+namespace xmlCreator
 {
-public:
-    AdbExpr();
-    ~AdbExpr();
 
-    static const char* statusStr(int status);
-    int  ResolveName(char *name, u_int64_t *val);
-    void Error(const std::string& message);
-    void setVars(map<string, string> *varsMap);
+  template <class T>
+  bool compareFieldsPtr(T *f1, T *f2);
+  string indentString(int i);
+  int dword(int bits);
+  int startBit(int bits);
+  string formatAddr(u_int32_t offs, u_int32_t size);
+  string encodeXml(const string &data);
+  string descNativeToXml(const string &desc);
 
-private:
-    map<string, string> *_varsMap;
-};
+}
 
-#endif // ADB_EXPR_H
+template <class T>
+bool xmlCreator::compareFieldsPtr(T *f1, T *f2)
+{
+    return (*f1) < (*f2);
+}
+
+#endif
