42cf85828f3296d2da87588d7020b7c41385a838
mahailiang <mahailiang@uniontech.com>
2022-04-27 21:58:33 +0800

    Add loongarch support for mstflint

--- mtcr_ul/packets_common.h
+++ mtcr_ul/packets_common.h
@@ -158,6 +158,8 @@
 #       define ARCH_arm6l
 #   elif defined(__riscv)
 #       define ARCH_riscv
+#   elif defined(__loongarch64)
+#       define ARCH_loongarch64
 #   else
 #       error Unknown CPU architecture using the linux OS
 #   endif
@@ -171,7 +173,7 @@
 /* define macros for print fields */
 //#if defined (ARCH_ia64) || defined(ARCH_x86_64) || defined(ARCH_ppc64) || defined(__MINGW64__)
 /*
- #if !defined(UEFI_BUILD) && (defined (ARCH_ia64) || defined(ARCH_x86_64) || defined(ARCH_ppc64) || defined(__MINGW64__) || defined(ARCH_riscv))
+ #if !defined(UEFI_BUILD) && (defined (ARCH_ia64) || defined(ARCH_x86_64) || defined(ARCH_ppc64) || defined(__MINGW64__) || defined(ARCH_riscv) || defined(ARCH_loongarch64))
  #   define U64H_FMT "0x%016lx"
  #   define U64D_FMT "%lu"
  #   define U32H_FMT "0x%08x"
