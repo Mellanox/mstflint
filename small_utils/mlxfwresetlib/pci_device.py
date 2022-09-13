f32f56a82d4546f9bc642343a06efd900ea3cbc1
Matan Eliyahu <matanel@nvidia.com>
2022-09-13 11:59:46 +0300

    Revert "Sync 1 as default in case of DPU and driver sync supported according to MCAM"
    This reverts commit cda200fa8ac6f2464125629125984d14f21e9709.

--- small_utils/mlxfwresetlib/pci_device.py
+++ small_utils/mlxfwresetlib/pci_device.py
@@ -39,7 +39,7 @@ from __future__ import print_function
 
 from mtcr import MstDevice
 from .mcra import Mcra
-# TODO fix it later - need to import the function from mlxfwreset.py
+# TODO fix it latter - need to import the function from mlxfwreset.py
 import sys
 import os
 mlxfwreset_dir = os.path.dirname(os.path.dirname(__file__))
