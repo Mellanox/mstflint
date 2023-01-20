578ed6d38ad7ef594b550466198f615f130ca65c
Alex Blago <alexbl@nvidia.com>
2023-01-20 01:23:37 -0800

    Add a header guard to the generated git version header

--- gitversion.sh
+++ gitversion.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
-if git rev-parse --short HEAD > gitversion.tmp
-then
-    echo "#define TOOLS_GIT_SHA \"`tr -d '\n' < gitversion.tmp`\"" > $1 
-fi
-rm gitversion.tmp
+OUT=${1:?Error: filename is missing.}
+
+git_sha=$(git rev-parse --short HEAD) || git_sha="undefined"
+
+cat > "$OUT" <<EOL
+#ifndef TOOLS_GIT_SHA_H
+#define TOOLS_GIT_SHA_H
+#define TOOLS_GIT_SHA "$git_sha"
+#endif
+EOL
