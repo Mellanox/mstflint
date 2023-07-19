99ebd55e88e5721c7c77345e01894980c8f1ab76
HarelKarni <99645047+HarelKarni@users.noreply.github.com>
2023-07-19 09:16:43 +0300

    Update reg_access_switch_layouts.c

--- tools_layouts/reg_access_switch_layouts.c
+++ tools_layouts/reg_access_switch_layouts.c
@@ -33,10 +33,8 @@
  
 
 /***
-         *** This file was generated at "2023-07-16 15:06:38"
-         *** by:
-         ***    > /auto/mswg/release/tools/a-me/a-me-1.2.4/a-me-1.2.4-24/adabe_plugins/adb2c/adb2pack.py --input adb/prm/switch/ext/reg_access_switch.adb --file-prefix reg_access_switch --prefix reg_access_switch_ --no-adb-utils -o /auto/swgwork1/hkarni/workspace/prm_update_mft/user/tools_layouts
-         ***/
+*** This file was generated at "2023-07-16 15:06:38"
+***/
 #include "reg_access_switch_layouts.h"
 
 void reg_access_switch_command_payload_ext_pack(const struct reg_access_switch_command_payload_ext *ptr_struct, u_int8_t *ptr_buff)
