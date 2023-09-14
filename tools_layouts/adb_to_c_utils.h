a27f1ea2c6e851f905e7b587a0c329de5a865f9d
Tianhao Chai <cth451@gmail.com>
2023-09-14 02:00:39 +0000

    hardware support: linux on loongarch64

--- tools_layouts/adb_to_c_utils.h
+++ tools_layouts/adb_to_c_utils.h
@@ -146,6 +146,8 @@ extern "C"
 #define ARCH_arm6l
 #elif defined(__riscv)
 #define ARCH_riscv
+#elif defined(__loongarch_lp64)
+#define ARCH_loongarch64
 #else
 #error Unknown CPU architecture using the linux OS
 #endif
@@ -188,7 +190,7 @@ extern "C"
 #define U64H_FMT "0x%016llx"
 #define U48H_FMT "0x%012llx"
 #endif
-#elif defined(ARCH_ia64) || defined(ARCH_x86_64) || defined(ARCH_ppc64) || defined(ARCH_arm64) || defined(ARCH_riscv)
+#elif defined(ARCH_ia64) || defined(ARCH_x86_64) || defined(ARCH_ppc64) || defined(ARCH_arm64) || defined(ARCH_riscv) || defined(ARCH_loongarch64)
 #define U64D_FMT "%lu"
 #define U64H_FMT "0x%016lx"
 #define U48H_FMT "0x%012lx"
