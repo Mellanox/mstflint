c380276a99ddbfeb6be2b7ef82c5320f8c0a57f8
AlexBlago <alexbl@nvidia.com>
2023-10-11 00:25:00 -0700

    common: Eliminate regex dependency on boost

--- common/regex.h
+++ common/regex.h
@@ -1,7 +1,11 @@
 #ifndef MSTFLINT_REGEX_H
 #define MSTFLINT_REGEX_H
 
+#ifdef USE_BOOST_REGEX
 #include <boost/regex.hpp>
+#else
+#include <regex>
+#endif
 
 namespace mstflint
 {
@@ -9,12 +13,21 @@ namespace common
 {
 namespace regex
 {
+#ifdef USE_BOOST_REGEX
 using boost::regex;
 using boost::match_results;
 using boost::regex_search;
 using boost::smatch;
 using boost::regex_replace;
 using boost::regex_match;
+#else
+using std::regex;
+using std::match_results;
+using std::regex_search;
+using std::smatch;
+using std::regex_replace;
+using std::regex_match;
+#endif
 
 } // namespace regex
 } // namespace common
