5599e78b64f8608cae77f032cf21c131db41562d
Harel Karni <hkarni@nvidia.com>
2025-04-07 12:57:24 +0300

    mlxlink | bug fix for show_fec is not supported on GB100 (B300 setup)
    Description:
    
    MSTFlint port needed:
    Tested OS:
    Tested devices:
    Tested flows:
    
    Known gaps (with RM ticket):
    
    Issue:

--- mlxlink/modules/mlxlink_commander.cpp
+++ mlxlink/modules/mlxlink_commander.cpp
@@ -4986,7 +4986,7 @@ bool MlxlinkCommander::isPPHCRSupported()
 
     try
     {
-        sendPrmReg(ACCESS_REG_PPHCR, GET, "local_port=1,pnat=%d", pnat);
+        sendPrmReg(ACCESS_REG_PPHCR, GET, "pnat=%d", pnat);
     }
     catch(MlxRegException & exc)
     {
