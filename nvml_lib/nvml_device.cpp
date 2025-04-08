1b137b7a44f4a3dd1dc793c8a347eb410b049927
Oded Burstein <oburstein@nvidia.com>
2025-04-08 16:33:56 +0300

    added missing registers for NVML

--- nvml_lib/nvml_device.cpp
+++ nvml_lib/nvml_device.cpp
@@ -112,13 +112,13 @@ int NvmlDevice::SendPRMRegister(unsigned char    * registerData,
         registerStatus = AccessRegisterMTCAP(registerData, registerSize, prmBuffer);
         break;
 
-    /* case REG_ID_MORD: */
-    /*     registerStatus = AccessRegisterMORD(registerData, registerSize, prmBuffer); */
-    /*     break; */
+    case REG_ID_MORD:
+        registerStatus = AccessRegisterMORD(registerData, registerSize, prmBuffer);
+        break;
 
-    /* case REG_ID_MCAM: */
-    /*     registerStatus = AccessRegisterMCAM(registerData, registerSize, prmBuffer); */
-    /*     break; */
+    case REG_ID_MCAM:
+        registerStatus = AccessRegisterMCAM(registerData, registerSize, prmBuffer);
+        break;
 
     default:
         throw NvmlException("NVML does not support register ID 0x%x\n", registerID);
@@ -619,91 +619,91 @@ int NvmlDevice::AccessRegisterSLTP(unsigned char    * registerData,
     }
 }
 
-/* int NvmlDevice::AccessRegisterMORD(unsigned char    * registerData, */
-/*                                    const unsigned int registerSize, */
-/*                                    nvmlPRMBuffer_t&   prmBuffer) */
-/* { */
-/*     nvmlReturn_t error = NVML_SUCCESS; */
+int NvmlDevice::AccessRegisterMORD(unsigned char    * registerData,
+                                   const unsigned int registerSize,
+                                   nvmlPRMBuffer_t&   prmBuffer)
+{
+    nvmlReturn_t error = NVML_SUCCESS;
 
-/*     / * Set PRM Register version & ID. * / */
-/*     prmBuffer.mord.version = nvmlMORD_v1; */
-/*     prmBuffer.regId = NVML_GPU_PRM_MORD; */
+    /* Set PRM Register version & ID. */
+    prmBuffer.mord.version = nvmlMORD_v1;
+    prmBuffer.regId = NVML_GPU_PRM_MORD;
 
-/*     / * convert the raw data buffer to HCA PRM struct. * / */
-/*     reg_access_hca_resource_dump_ext mordHcaReg; */
+    /* convert the raw data buffer to HCA PRM struct. */
+    reg_access_hca_resource_dump_ext mordHcaReg;
 
-/*     memset(&mordHcaReg, 0, sizeof(mordHcaReg)); */
-/*     reg_access_hca_resource_dump_ext_unpack(&mordHcaReg, registerData); */
-/*     prmBuffer.mord.segmentType = mordHcaReg.segment_type; */
-/*     prmBuffer.mord.seqNum = mordHcaReg.seq_num; */
-/*     prmBuffer.mord.vhcaIdValid = mordHcaReg.vhca_id_valid; */
-/*     prmBuffer.mord.inlineDump = mordHcaReg.inline_dump; */
-/*     prmBuffer.mord.vhcaId = mordHcaReg.vhca_id; */
-/*     prmBuffer.mord.index1 = mordHcaReg.index1; */
-/*     prmBuffer.mord.index2 = mordHcaReg.index2; */
-/*     prmBuffer.mord.numOfObj2 = mordHcaReg.num_of_obj2; */
-/*     prmBuffer.mord.numOfObj1 = mordHcaReg.num_of_obj1; */
-/*     prmBuffer.mord.deviceOpaque = mordHcaReg.device_opaque; */
-/*     prmBuffer.mord.mkey = mordHcaReg.mkey; */
+    memset(&mordHcaReg, 0, sizeof(mordHcaReg));
+    reg_access_hca_resource_dump_ext_unpack(&mordHcaReg, registerData);
+    prmBuffer.mord.segmentType = mordHcaReg.segment_type;
+    prmBuffer.mord.seqNum = mordHcaReg.seq_num;
+    prmBuffer.mord.vhcaIdValid = mordHcaReg.vhca_id_valid;
+    prmBuffer.mord.inlineDump = mordHcaReg.inline_dump;
+    prmBuffer.mord.vhcaId = mordHcaReg.vhca_id;
+    prmBuffer.mord.index1 = mordHcaReg.index1;
+    prmBuffer.mord.index2 = mordHcaReg.index2;
+    prmBuffer.mord.numOfObj2 = mordHcaReg.num_of_obj2;
+    prmBuffer.mord.numOfObj1 = mordHcaReg.num_of_obj1;
+    prmBuffer.mord.deviceOpaque = mordHcaReg.device_opaque;
+    prmBuffer.mord.mkey = mordHcaReg.mkey;
 
-/*     / * Send the PRM to the driver. * / */
-/*     error = _nvmlLibWrapper->nvmlDeviceReadWritePRM(_nvmlDevice, &prmBuffer); */
-/*     if (error) { */
-/*         std::cout << "Error sending MORD: " << _nvmlLibWrapper->nvmlErrorString(error); */
-/*     } */
+    /* Send the PRM to the driver. */
+    error = _nvmlLibWrapper->nvmlDeviceReadWritePRM(_nvmlDevice, &prmBuffer);
+    if (error) {
+        std::cout << "Error sending MORD: " << _nvmlLibWrapper->nvmlErrorString(error);
+    }
 
-/*     / * Convert the IOCTL Data to GPUNet struct. * / */
-/*     memset(&mordHcaReg, 0, sizeof(mordHcaReg)); */
-/*     reg_access_gpu_resource_dump_ext mordGpuReg; */
+    /* Convert the IOCTL Data to GPUNet struct. */
+    memset(&mordHcaReg, 0, sizeof(mordHcaReg));
+    reg_access_gpu_resource_dump_ext mordGpuReg;
 
-/*     memset(&mordGpuReg, 0, sizeof(mordGpuReg)); */
-/*     reg_access_gpu_resource_dump_ext_unpack(&mordGpuReg, prmBuffer.data); */
+    memset(&mordGpuReg, 0, sizeof(mordGpuReg));
+    reg_access_gpu_resource_dump_ext_unpack(&mordGpuReg, prmBuffer.data);
 
-/*     / * Convert the GPUNet struct to original HCA struct. * / */
-/*     mordHcaReg.segment_type = mordGpuReg.segment_type; */
-/*     mordHcaReg.seq_num = mordGpuReg.seq_num; */
-/*     mordHcaReg.inline_dump = mordGpuReg.inline_dump; */
-/*     mordHcaReg.more_dump = mordGpuReg.more_dump; */
-/*     mordHcaReg.index1 = mordGpuReg.index1; */
-/*     mordHcaReg.index2 = mordGpuReg.index2; */
-/*     mordHcaReg.num_of_obj2 = mordGpuReg.num_of_obj2; */
-/*     mordHcaReg.num_of_obj1 = mordGpuReg.num_of_obj1; */
-/*     mordHcaReg.device_opaque = mordGpuReg.device_opaque; */
-/*     mordHcaReg.size = mordGpuReg.size; */
-/*     mordHcaReg.vhca_id_valid = mordGpuReg.vhca_id_valid; */
-/*     mordHcaReg.vhca_id = mordGpuReg.vhca_id; */
-/*     memcpy(mordHcaReg.inline_data, mordGpuReg.inline_data, sizeof(mordGpuReg.inline_data)); */
+    /* Convert the GPUNet struct to original HCA struct. */
+    mordHcaReg.segment_type = mordGpuReg.segment_type;
+    mordHcaReg.seq_num = mordGpuReg.seq_num;
+    mordHcaReg.inline_dump = mordGpuReg.inline_dump;
+    mordHcaReg.more_dump = mordGpuReg.more_dump;
+    mordHcaReg.index1 = mordGpuReg.index1;
+    mordHcaReg.index2 = mordGpuReg.index2;
+    mordHcaReg.num_of_obj2 = mordGpuReg.num_of_obj2;
+    mordHcaReg.num_of_obj1 = mordGpuReg.num_of_obj1;
+    mordHcaReg.device_opaque = mordGpuReg.device_opaque;
+    mordHcaReg.size = mordGpuReg.size;
+    mordHcaReg.vhca_id_valid = mordGpuReg.vhca_id_valid;
+    mordHcaReg.vhca_id = mordGpuReg.vhca_id;
+    memcpy(mordHcaReg.inline_data, mordGpuReg.inline_data, sizeof(mordGpuReg.inline_data));
 
-/*     reg_access_hca_resource_dump_ext_pack(&mordHcaReg, registerData); */
-/*     return error; */
-/* } */
+    reg_access_hca_resource_dump_ext_pack(&mordHcaReg, registerData);
+    return error;
+}
 
 
-/* int NvmlDevice::AccessRegisterMCAM(unsigned char    * registerData, */
-/*                                    const unsigned int registerSize, */
-/*                                    nvmlPRMBuffer_t&   prmBuffer) */
-/* { */
-/*     nvmlReturn_t error = NVML_SUCCESS; */
+int NvmlDevice::AccessRegisterMCAM(unsigned char    * registerData,
+                                   const unsigned int registerSize,
+                                   nvmlPRMBuffer_t&   prmBuffer)
+{
+    nvmlReturn_t error = NVML_SUCCESS;
 
-/*     / * Set PRM Register version & ID. * / */
-/*     prmBuffer.mcam.version = nvmlMCAM_v1; */
-/*     prmBuffer.regId = NVML_GPU_PRM_MCAM; */
+    /* Set PRM Register version & ID. */
+    prmBuffer.mcam.version = nvmlMCAM_v1;
+    prmBuffer.regId = NVML_GPU_PRM_MCAM;
 
-/*     / * convert the raw data buffer to PRM struct. * / */
-/*     reg_access_gpu_mcam_reg_ext mcamReg; */
+    /* convert the raw data buffer to PRM struct. */
+    reg_access_gpu_mcam_reg_ext mcamReg;
 
-/*     memset(&mcamReg, 0, sizeof(mcamReg)); */
-/*     reg_access_gpu_mcam_reg_ext_unpack(&mcamReg, registerData); */
-/*     prmBuffer.mcam.accessRegGroup = mcamReg.access_reg_group; */
-/*     prmBuffer.mcam.featureGroup = mcamReg.feature_group; */
+    memset(&mcamReg, 0, sizeof(mcamReg));
+    reg_access_gpu_mcam_reg_ext_unpack(&mcamReg, registerData);
+    prmBuffer.mcam.accessRegGroup = mcamReg.access_reg_group;
+    prmBuffer.mcam.featureGroup = mcamReg.feature_group;
 
-/*     / * Send the PRM to the driver. * / */
-/*     error = _nvmlLibWrapper->nvmlDeviceReadWritePRM(_nvmlDevice, &prmBuffer); */
-/*     if (error) { */
-/*         std::cout << "Error sending MCAM: " << _nvmlLibWrapper->nvmlErrorString(error); */
-/*     } */
+    /* Send the PRM to the driver. */
+    error = _nvmlLibWrapper->nvmlDeviceReadWritePRM(_nvmlDevice, &prmBuffer);
+    if (error) {
+        std::cout << "Error sending MCAM: " << _nvmlLibWrapper->nvmlErrorString(error);
+    }
 
-/*     / * Copy back the register data. * / */
-/*     memcpy(registerData, prmBuffer.data, registerSize); */
-/*     return error; */
-/* } */
+    /* Copy back the register data. */
+    memcpy(registerData, prmBuffer.data, registerSize);
+    return error;
+}
