939154a06b329e6766ee1625b2969687146e58cd
Oded Burstein <oburstein@nvidia.com>
2025-06-29 10:52:54 +0300

    [NMVL] updated the PRM Access API

--- nvml_lib/nvml_lib_wrapper.cpp
+++ nvml_lib/nvml_lib_wrapper.cpp
@@ -7,11 +7,7 @@
 
 NvmlLibWrapper::NvmlLibWrapper(const string libPath) : _libPath(libPath)
 {
-    _libraryHandle = dlopen(_libPath.c_str(), RTLD_LAZY);
-    if (!_libraryHandle) {
-        throw NvmlException("Failed to load libnvidia-ml.so, %s\n", dlerror());
-    }
-
+    LoadDynamicLib();
     LoadDynamicFuncs();
     InitNvmlSDK();
 }
@@ -19,11 +15,7 @@ NvmlLibWrapper::NvmlLibWrapper(const string libPath) : _libPath(libPath)
 NvmlLibWrapper::~NvmlLibWrapper()
 {
     nvmlShutdown();
-
-    if (_libraryHandle) {
-        dlclose(_libraryHandle);
-        _libraryHandle = nullptr;
-    }
+    FreeDynamicLib();
 }
 
 
@@ -31,8 +23,25 @@ void NvmlLibWrapper::InitNvmlSDK()
 {
     nvmlReturn_t error = nvmlInit_v2();
 
-    if (error) {
+    if (error)
+    {
         throw NvmlException("Error initializing NVML SDK: %s\n", nvmlErrorString(error));
+    }
+}
+
+void NvmlLibWrapper::LoadDynamicLib()
+{
+    _libraryHandle = dlopen(_libPath.c_str(), RTLD_LAZY);
+    if (!_libraryHandle) {
+        throw NvmlException("Failed to load libnvidia-ml.so, %s\n", dlerror());
+    }
+}
+
+void NvmlLibWrapper::FreeDynamicLib()
+{
+    if (_libraryHandle) {
+        dlclose(_libraryHandle);
+        _libraryHandle = nullptr;
     }
 }
 
@@ -52,10 +61,21 @@ void* NvmlLibWrapper::LoadDynamicFunc(const std::string& functionName)
 
 void NvmlLibWrapper::LoadDynamicFuncs()
 {
-    nvmlInit_v2 = (f_nvmlInit_v2)LoadDynamicFunc("nvmlInit_v2");
-    nvmlShutdown = (f_nvmlShutdown)LoadDynamicFunc("nvmlShutdown");
-    nvmlDeviceGetHandleByIndex_v2 = (f_nvmlDeviceGetHandleByIndex_v2)LoadDynamicFunc("nvmlDeviceGetHandleByIndex_v2");
-    nvmlDeviceGetPciInfo_v3 = (f_nvmlDeviceGetPciInfo_v3)LoadDynamicFunc("nvmlDeviceGetPciInfo_v3");
-    nvmlErrorString = (f_nvmlErrorString)LoadDynamicFunc("nvmlErrorString");
-    nvmlDeviceReadWritePRM = (f_nvmlDeviceReadWritePRM)LoadDynamicFunc("nvmlDeviceReadWritePRM");
+    try
+    {
+        nvmlInit = (f_nvmlInit_v2)LoadDynamicFunc("nvmlInit_v2");
+        nvmlShutdown = (f_nvmlShutdown)LoadDynamicFunc("nvmlShutdown");
+        nvmlDeviceGetCount = (f_nvmlDeviceGetCount_v2)LoadDynamicFunc("nvmlDeviceGetCount_v2");
+        nvmlDeviceGetHandleByIndex =
+          (f_nvmlDeviceGetHandleByIndex_v2)LoadDynamicFunc("nvmlDeviceGetHandleByIndex_v2");
+        nvmlDeviceGetPciInfo =
+          (f_nvmlDeviceGetPciInfo_v3)LoadDynamicFunc("nvmlDeviceGetPciInfo_v3");
+        nvmlErrorString = (f_nvmlErrorString)LoadDynamicFunc("nvmlErrorString");
+        nvmlDeviceReadWritePRM =
+          (f_nvmlDeviceReadWritePRM)LoadDynamicFunc("nvmlDeviceReadWritePRM_v1");
+    }
+    catch (const std::exception& e)
+    {
+         throw NvmlException(e.what() + string("\nPlease make sure you are using an up to date version of NVML lib."));
+    }
 }
