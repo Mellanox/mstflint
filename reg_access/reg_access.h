90be845367be0fde80a842e2ec6a071df02539ef
Moriya Pilipenko <mpilipenko@nvidia.com>
2025-07-02 14:59:04 +0300

    mlxfwreset | Incorrect detection of the PCIe switch when it acts as a switch only
    Description: Using MPIR to compare device bus to MPIR bus isn't working as a means of PCIe switch detcetion when device is SD or PCIe switch only. Instead using MPEIN to check if the port type is that of PCI_EXPRESS_UPSTREAM_PORT (meaning we should be in PCIe switch flow).
    
    Issue: 4503900
    Issue: 4517447

--- reg_access/reg_access.h
+++ reg_access/reg_access.h
@@ -282,6 +282,10 @@ reg_access_status_t reg_access_mpir(mfile* mf, reg_access_method_t method, struc
 struct reg_access_hca_MRSV_ext;
 reg_access_status_t reg_access_mrsv(mfile* mf, reg_access_method_t method, struct reg_access_hca_MRSV_ext* mrsv);
 
+struct reg_access_hca_mpein_reg_ext;
+    reg_access_status_t
+      reg_access_mpein(mfile* mf, reg_access_method_t method, struct reg_access_hca_mpein_reg_ext* mpein);
+
 #ifdef __cplusplus
 }
 #endif
