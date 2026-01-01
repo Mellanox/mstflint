d263b3b2c96a7dc870fb2fc6aa1ed7e97011d7fd
Sahar Vaya <svaya@apps-63.mtl.labs.mlnx>
2026-01-01 16:49:37 +0200

    [fwctl] throwing correct fwctrl error messages
    Description: There was an uncorrect error messages parsing from FW. changed the error message parsing to the correct errors.
    The enum was taken from here:
    https://github.com/torvalds/linux/blob/master/include/linux/mlx5/device.h
    In addition, there is a reference under Adapters PRM section 34.2 Return Status Summary
    
    mstflint port needed: Yes
    Tested OS: Linux
    Tested devices: fwctl
    Tested flows: mlxreg -d fwctl2 --reg_name MNVIA --get, mlxreg -d fwctl2 --reg_name SLRG --get
    
    Known gaps (with RM ticket):
    
    Issue: 4610385 4610369

--- mtcr_ul/fwctrl.c
+++ mtcr_ul/fwctrl.c
@@ -48,22 +48,6 @@
 #include "fwctrl.h"
 #include "fwctrl_ioctl.h"
 
-enum CMD_ErrorCode {
-    CMD_ERROR_OK             = 0x00,
-    CMD_ERROR_INTERNAL_ERROR = 0x01,
-    CMD_ERROR_BAD_OP         = 0x02,
-    CMD_ERROR_BAD_PARAM      = 0x03,
-    CMD_ERROR_BAD_SYS_STATE  = 0x04,
-    CMD_ERROR_BAD_RESOURCE   = 0x05,
-    CMD_ERROR_RESOURCE_BUSY  = 0x06,
-    CMD_ERROR_NOT_READY      = 0x07,
-    CMD_ERROR_EXCEED_LIM     = 0x08,
-    CMD_ERROR_BAD_RES_STATE  = 0x09,
-    CMD_ERROR_BAD_INDEX      = 0x0A,
-    CMD_ERROR_NO_RESOURCES   = 0x0F,
-    CMD_ERROR_BAD_INPUT_LEN  = 0x50,
-    CMD_ERROR_BAD_OUTPUT_LEN = 0x51
-};
 
 void fwctl_set_device_id(mfile* mf)
 {
@@ -85,53 +69,64 @@ void fwctl_set_device_id(mfile* mf)
 
 int translate_cmd_status_to_reg_status(int cmd_status)
 {
-    switch (cmd_status) {
-    case CMD_ERROR_OK:
-        return ME_REG_ACCESS_OK;
+    switch (cmd_status)
+    {
+        case MLX5_CMD_STAT_OK:
+            return ME_REG_ACCESS_OK;
 
-    case CMD_ERROR_INTERNAL_ERROR:
-        return ME_REG_ACCESS_INTERNAL_ERROR;
+        case MLX5_CMD_STAT_INT_ERR:
+            return ME_REG_ACCESS_INTERNAL_ERROR;
 
-    case CMD_ERROR_BAD_OP:
-        return ME_REG_ACCESS_VER_NOT_SUPP;
+        case MLX5_CMD_STAT_BAD_OP_ERR:
+            return ME_REG_ACCESS_BAD_METHOD;
 
-    case CMD_ERROR_BAD_PARAM:
-        return ME_REG_ACCESS_UNKNOWN_TLV;
+        case MLX5_CMD_STAT_BAD_PARAM_ERR:
+            return ME_REG_ACCESS_BAD_PARAM;
 
-    case CMD_ERROR_BAD_SYS_STATE:
-        return ME_REG_ACCESS_REG_NOT_SUPP;
+        case MLX5_CMD_STAT_BAD_SYS_STATE_ERR:
+            return ME_REG_ACCESS_REG_NOT_SUPP;
 
-    case CMD_ERROR_BAD_RESOURCE:
-        return ME_REG_ACCESS_RES_NOT_AVLBL;
+        case MLX5_CMD_STAT_BAD_RES_ERR:
+            return ME_REG_ACCESS_RES_NOT_AVLBL;
 
-    case CMD_ERROR_RESOURCE_BUSY:
-        return ME_REG_ACCESS_DEV_BUSY;
+        case MLX5_CMD_STAT_RES_BUSY:
+            return ME_REG_ACCESS_DEV_BUSY;
 
-    case CMD_ERROR_NOT_READY:
-        return ME_REG_ACCESS_NOT_READY;
+        case MLX5_CMD_STAT_NOT_READY:
+            return ME_REG_ACCESS_NOT_READY;
 
-    case CMD_ERROR_EXCEED_LIM:
-        return ME_REG_ACCESS_EXCEED_LIM;
+        case MLX5_CMD_STAT_LIM_ERR:
+            return ME_REG_ACCESS_SIZE_EXCEEDS_LIMIT;
 
-    case CMD_ERROR_BAD_RES_STATE:
-        return ME_REG_ACCESS_BAD_RES_STATE;
+        case MLX5_CMD_STAT_BAD_RES_STATE_ERR:
+            return ME_REG_ACCESS_BAD_RES_STATE;
 
-    case CMD_ERROR_BAD_INDEX:
-        return ME_REG_ACCESS_BAD_INDEX;
+        case MLX5_CMD_STAT_IX_ERR:
+            return ME_REG_ACCESS_BAD_INDEX;
 
-    case CMD_ERROR_NO_RESOURCES:
-        return ME_REG_ACCESS_RES_NOT_AVLBL;
+        case MLX5_CMD_STAT_NO_RES_ERR:
+            return ME_REG_ACCESS_RES_NOT_AVLBL;
 
-    case CMD_ERROR_BAD_INPUT_LEN:
-        return ME_REG_ACCESS_BAD_INDEX;
+        case MLX5_CMD_STAT_BAD_INP_LEN_ERR:
+            return ME_REG_ACCESS_BAD_INPUT_LEN;
 
-    case CMD_ERROR_BAD_OUTPUT_LEN:
-        return ME_REG_ACCESS_BAD_INPUT_LEN;
+        case MLX5_CMD_STAT_BAD_OUTP_LEN_ERR:
+            return ME_REG_ACCESS_BAD_OUTPUT_LEN;
 
-    default:
-        return ME_REG_ACCESS_UNKNOWN_ERR;
+        case MLX5_CMD_STAT_BAD_QP_STATE_ERR:
+            return ME_REG_ACCESS_BAD_QP_STATE;
+
+        case MLX5_CMD_STAT_BAD_PKT_ERR:
+            return ME_REG_ACCESS_BAD_PKT;
+
+        case MLX5_CMD_STAT_BAD_SIZE_OUTS_CQES_ERR:
+            return ME_REG_ACCESS_BAD_SIZE_OUTS_CQES;
+
+        default:
+            return ME_REG_ACCESS_UNKNOWN_ERR;
     }
 }
+
 
 int fwctl_control_access_register(int    fd,
                                   void  *data_in,
@@ -193,11 +188,7 @@ int fwctl_control_access_register(int    fd,
         FWCTL_DEBUG_PRINT(mf, "FWCTL_IOCTL_CMD_RPC returned error from FW: reg_id=0x%x, method=0x%x, cmd_status=0x%x, syndrome=0x%x\n",
                           reg_id, method, cmd_status, syndrome);
 
-        if (reg_id == mnvda_reg_id) {
-            *reg_status = translate_cmd_status_to_reg_status(cmd_status);
-        } else {
-            *reg_status = return_by_reg_status(cmd_status);
-        }
+        *reg_status = translate_cmd_status_to_reg_status(cmd_status);
         FWCTL_DEBUG_PRINT(mf, "Mapped FW cmd_status=0x%x to reg_status=0x%x (%s)\n", cmd_status, *reg_status, m_err2str(*reg_status));
     } else {
         *reg_status = 0;
--- mtcr_ul/mtcr_common.c
+++ mtcr_ul/mtcr_common.c
@@ -191,6 +191,36 @@ const char* m_err2str(MError status)
 
     case ME_REG_ACCESS_INSUFFICIENT_PERMISSIONS:
             return "Unable to send PRM Register due to permission issue, debug token might be needed.";
+    
+    case ME_REG_ACCESS_BAD_QP_STATE:
+        return "Register access bad QP state";
+
+    case ME_REG_ACCESS_BAD_PKT:
+        return "Register access bad packet";
+
+    case ME_REG_ACCESS_BAD_SIZE_OUTS_CQES:
+        return "Register access bad size outs CQES";
+
+    case ME_REG_ACCESS_BAD_SYS_STATE:
+        return "Register access bad system state";
+
+    case ME_REG_ACCESS_EXCEED_LIM:
+        return "Register access exceed limit";
+
+    case ME_REG_ACCESS_BAD_RES_STATE:
+        return "Register access bad resource state";
+
+    case ME_REG_ACCESS_BAD_INDEX:
+        return "Register access bad index";
+
+    case ME_REG_ACCESS_BAD_INPUT_LEN:
+        return "Register access bad input length";
+
+    case ME_REG_ACCESS_BAD_OUTPUT_LEN:
+        return "Register access bad output length";
+
+    case ME_REG_ACCESS_NOT_READY:
+        return "Register access not ready";
 
     /* ICMD access errors */
     case ME_ICMD_STATUS_CR_FAIL:
