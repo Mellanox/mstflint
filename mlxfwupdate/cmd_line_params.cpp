27eeef802e8eef898ca842dd7848cb9c1976132b
Itay Avraham <itayav17@gmail.com>
2026-02-02 23:30:25 +0200

    mlxfwmanager | add a new flag to update FW in parallel using fwctl

--- mlxfwupdate/cmd_line_params.cpp
+++ mlxfwupdate/cmd_line_params.cpp
@@ -59,6 +59,7 @@ CmdLineParams::CmdLineParams()
     clear_semaphore = false;
     extract_all = false;
     no_fw_ctrl = false;
+    fw_update_in_parallel_via_fwctl = false;
     target_file = "";
     server_url = "https://www.mellanox.com";
     proxy = "";
