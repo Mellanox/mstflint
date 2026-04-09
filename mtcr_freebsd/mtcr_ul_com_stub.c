f412699de63604e58dc868e09ad4c68fb3fa6fb9
Moriya Pilipenko <mpilipenko@nvidia.com>
2026-04-09 16:11:56 +0300

    mstflint | freeBSD | compilation error
    - Add __BYTE_ORDER, __BIG_ENDIAN, __LITTLE_ENDIAN defines for FreeBSD
      in compatibility.h (FreeBSD uses BYTE_ORDER without leading underscores)
    - Add missing mtcr_ul_com.c file so FreeBSD build isn't missing hot_reset symbol

---
+++ mtcr_freebsd/mtcr_ul_com_stub.c
@@ -0,0 +1,13 @@
+#include "../include/mtcr_ul/mtcr.h"
+
+int hot_reset(mfile* mf,
+    int in_parallel,
+    int domain_1, int bus_1, int device_1, int function_1,
+    int domain_2, int bus_2, int device_2, int function_2)
+{
+    (void)mf;
+    (void)in_parallel;
+    (void)domain_1; (void)bus_1; (void)device_1; (void)function_1;
+    (void)domain_2; (void)bus_2; (void)device_2; (void)function_2;
+    return -1;
+}
