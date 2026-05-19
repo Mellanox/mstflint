104535793f45f0292960e8557cc89c58b936bfba
Shelly Sela <ssela@nvidia.com>
2026-05-19 12:46:35 +0300

    mstflint - block image reactivation in case the old(running) image has a diiferent PSID than the locked PSID
    Description: when the PSID major minor split FR is supported, if the
    user burns an image that has a different minor PSID, the new PSID is
    locked. if right after that he changes his mind and tried to reactivate
    the old image - a error message will be printed:
    
    mstflint -d /dev/mst/mt4133_pciconf0 ir
    -E- Image reactivation blocked: device PSID (MT_0000001590) does not match locked PSID (MT_0002001590).
    
    Tested OS:linux
    Tested devices:cx9
    Tested flows:
    mlxburn -d /dev/mst/mt4133_pciconf0  -fw /.autodirect/fwgwork1/oshrid/hca_fix_psid_bug/hca/fw-ConnectX9.mlx -c /tmp/ini.ini -y
    
    mstflint -d /dev/mst/mt4133_pciconf0 ir
    -E- Image reactivation blocked: device PSID (MT_0000001590) does not match locked PSID (MT_0002001590).
    
    Known gaps (with RM ticket): n/a
    
    Issue: 5033936

--- flint/subcommands.cpp
+++ flint/subcommands.cpp
@@ -5605,7 +5605,7 @@ FlintStatus ImageReactivationSubCommand::executeCommand()
     }
     if (!_fwOps->FwReactivateImage())
     {
-        reportErr(true, FLINT_FAILED_IMAGE_REACTIVATION_ERROR, _flintParams.device.c_str(), ops->err());
+        reportErr(true, FLINT_FAILED_IMAGE_REACTIVATION_ERROR, _flintParams.device.c_str(), _fwOps->err());
         return FLINT_FAILED;
     }
     printf("\n-I- FW Image Reactivation succeeded.\n\n");
