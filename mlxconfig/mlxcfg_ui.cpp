Mellanox/mstflint

commit ba9b0e1085ba297ce096d5ad17be46b444aad886

[mlxconfig][pldm] Support reading NV config from PLDM package
Description:
mlxconfig apply now accepts a PLDM firmware package as input in addition
to a raw binary NV config file — the tool transparently detects the format
and extracts the NVCONFIG component when a PLDM package is provided.

MSTFlint port needed: yes
Tested OS: Linux
Tested devices: /dev/mst/mt4131_pciconf0
Tested flows: mlxconfig -d /dev/mst/mt4131_pciconf0 -t HCA apply <pldm_file>

Known gaps (with RM ticket):

Issue: 5024310
Change-Id: I1e2314204a434f4e2090b7b5e485c638f314b531

mlxconfig/Makefile.am
@@ -44,6 +44,7 @@ AM_CPPFLAGS = \
     -I$(top_srcdir)/dev_mgt \
     -I$(top_srcdir)/cmdif \
     -I$(top_srcdir)/tools_res_mgmt \
+    -I$(top_srcdir)/mflash \
     $(libxml2_CFLAGS) \
     $(openssl_CFLAGS)
 
@@ -89,6 +90,12 @@ libmlxcfg_la_LIBADD = $(libmlxcfg_la_DEPENDENCIES) ${LDL}
 
 mstconfig_DEPENDENCIES = \
     libmlxcfg.la \
+    $(top_builddir)/mlxfwops/lib/libmlxfwops.a \
+    $(top_builddir)/pldmlib/libpldm.la \
+    $(top_builddir)/libmfa/libmfa.la \
+    $(top_builddir)/ext_libs/minixz/libminixz.la \
+    $(top_builddir)/mflash/libmflash.la \
+    $(top_builddir)/tools_crypto/libtools_crypto.a \
     $(top_builddir)/mft_utils/libmftutils.la \
     $(top_builddir)/cmdif/libcmdif.la \
     $(top_builddir)/reg_access/libreg_access.la \
@@ -125,6 +132,10 @@ mstconfig_LDADD += $(top_builddir)/mlxsign_lib/libmlxsign.la $(openssl_LIBS)
 libmlxcfg_la_LIBADD += $(top_builddir)/mlxsign_lib/libmlxsign.la $(openssl_LIBS)
 else
 AM_CXXFLAGS += -DNO_OPEN_SSL
+endif
+
+if ENABLE_DPA
+mstconfig_DEPENDENCIES += $(top_builddir)/mlxdpa/libmstdpa.a
 endif
 
 mstconfig_SOURCES =  mlxcfg_ui.h mlxcfg_parser.cpp mlxcfg_ui.cpp

mlxconfig/mlxcfg_ui.cpp
@@ -56,6 +56,7 @@
 #include "mlxcfg_utils.h"
 #include "mft_utils/mft_utils.h"
 #include "common/tools_string.h"
+#include "mlxfwops/lib/fs_pldm.h"
 
 using nbu::mft::common::string_format;
 
@@ -1586,6 +1587,17 @@ mlxCfgStatus MlxCfg::readBinFile(string fileName, vector<u_int32_t>& buff)
 
 mlxCfgStatus MlxCfg::readNVInputFile(vector<u_int32_t>& buff)
 {
+    if (FwOperations::IsPLDM(_mlxParams.NVInputFile))
+    {
+        if (FsPldmOperations::GetComponentData(
+              _mlxParams.NVInputFile,
+              {ComponentIdentifier::Identifier_OEM_NVCONFIG_Comp, ComponentIdentifier::Identifier_MLNX_NVCONFIG_Comp},
+              buff))
+        {
+            return MLX_CFG_OK;
+        }
+        return err(true, "Failed to read PLDM file: %s", _mlxParams.NVInputFile.c_str());
+    }
     return readBinFile(_mlxParams.NVInputFile, buff);
 }
 

mlxfwops/lib/fw_ops.h
@@ -316,6 +316,7 @@ public:
     virtual bool GetSecureHostState(u_int8_t& state);
     virtual bool ChangeSecureHostState(bool disable, u_int64_t key);
     virtual bool IsComponentSupported(FwComponent::comps_ids_t component);
+    static bool IsPLDM(const string& pldmFile);
     virtual bool getBFBComponentsVersions(std::map<std::string, std::string>& name_to_version, bool pending);
     void getSupportedHwId(u_int32_t** supportedHwId, u_int32_t& supportedHwIdNum);
     
@@ -710,7 +711,6 @@ protected:
     virtual bool VerifyBranchFormat(const char* vsdString);
     virtual bool GetDtocAddress(u_int32_t& dTocAddress);
     virtual bool IsVmodSupported();
-    static bool IsPLDM(const string& pldmFile);
 
     // Protected Members
     FBase* _ioAccess;

commit 30b81a780290583bad77a3ba77e179aefbf217ee

[mlxconfig] Fix build break introduced by ba9b0e1
Description:
Replace FwOperations::IsPLDM() with a local 16-byte header check
(isPldmFile) to avoid pulling fw_ops.o and its transitive DPA
dependency into the mstconfig link. Add libmlxfwops.a with
--gc-sections to prune the dead FsPldmOperations vtable chain, and
move mlxfwops before mlxconfig in SUBDIRS to satisfy build order.

MSTFlint port needed:
Tested OS: Linux
Tested devices:
Tested flows: ./autogen.sh && ./configure [10 flag permutations] && make -j

Known gaps (with RM ticket):

Issue: 5103587

Makefile.am
@@ -28,7 +28,7 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-SUBDIRS = $(NVML_DIR) mft_core ext_libs $(TOOLS_CRYPTO) tools_layouts common $(VFIO_DRIVER_DIR) ${MTCR_CONF_DIR} mtcr_py $(MAD_IFC) $(XZ_UTILS_DIR) reg_access cmdif dev_mgt pci_library mft_utils $(CABLE_ACCESS_DIR) tools_res_mgmt mvpd mflash fw_comps_mgr libmfa pldm_utils pldmlib cmdparser mlxconfig $(NVFWRESET_DIR) $(DPA) mlxfwops $(FW_MGR_TOOLS) $(PLDM_PKG_GEN) mlxtokengenerator flint small_utils mst_utils mstdump ${ADABE_TOOLS} tracers resourcetools $(IBDUMP_DIR)
+SUBDIRS = $(NVML_DIR) mft_core ext_libs $(TOOLS_CRYPTO) tools_layouts common $(VFIO_DRIVER_DIR) ${MTCR_CONF_DIR} mtcr_py $(MAD_IFC) $(XZ_UTILS_DIR) reg_access cmdif dev_mgt pci_library mft_utils $(CABLE_ACCESS_DIR) tools_res_mgmt mvpd mflash fw_comps_mgr libmfa pldm_utils pldmlib cmdparser mlxfwops mlxconfig $(NVFWRESET_DIR) $(DPA) $(FW_MGR_TOOLS) $(PLDM_PKG_GEN) mlxtokengenerator flint small_utils mst_utils mstdump ${ADABE_TOOLS} tracers resourcetools $(IBDUMP_DIR)
 
 DIST_SUBDIRS = tracers
 

mlxconfig/Makefile.am
@@ -110,7 +110,7 @@ mstconfig_DEPENDENCIES = \
 
 mstconfig_LDADD = $(mstconfig_DEPENDENCIES) ${LDL}
 
-mstconfig_LDFLAGS = -static
+mstconfig_LDFLAGS = -static -Wl,--gc-sections
 
 if DISABLE_XML2
 AM_CXXFLAGS += -DDISABLE_XML2

mlxconfig/mlxcfg_ui.cpp
@@ -57,6 +57,7 @@
 #include "mft_utils/mft_utils.h"
 #include "common/tools_string.h"
 #include "mlxfwops/lib/fs_pldm.h"
+#include "pldm_utils/pldm_utils.h"
 
 using nbu::mft::common::string_format;
 
@@ -1585,9 +1586,24 @@ mlxCfgStatus MlxCfg::readBinFile(string fileName, vector<u_int32_t>& buff)
     return MLX_CFG_OK;
 }
 
+bool MlxCfg::isPldmFile(const string& path)
+{
+    std::ifstream ifs(path.c_str(), std::ios::in | std::ios::binary);
+    u_int8_t header[16] = {0};
+    if (ifs)
+        ifs.read(reinterpret_cast<char*>(header), sizeof(header));
+    for (const auto& entry : PACKAGE_HEADER_FORMAT_REVISION_MAP)
+    {
+        const std::vector<u_int8_t>& id = entry.second;
+        if (std::equal(id.begin(), id.end(), header))
+            return true;
+    }
+    return false;
+}
+
 mlxCfgStatus MlxCfg::readNVInputFile(vector<u_int32_t>& buff)
 {
-    if (FwOperations::IsPLDM(_mlxParams.NVInputFile))
+    if (isPldmFile(_mlxParams.NVInputFile))
     {
         if (FsPldmOperations::GetComponentData(
               _mlxParams.NVInputFile,

mlxconfig/mlxcfg_ui.h
@@ -245,6 +245,7 @@ private:
     mlxCfgStatus clrDevSem();
 
     mlxCfgStatus readBinFile(string fileName, vector<u_int32_t>& buff);
+    static bool isPldmFile(const string& path);
     mlxCfgStatus readNVInputFile(vector<u_int32_t>& buff);
     mlxCfgStatus readNVInputFile(string& content);
     mlxCfgStatus readNVInputFile(vector<string>& lines);

mlxfwops/lib/Makefile.am
@@ -101,9 +101,9 @@ libmlxfwops_a_SOURCES = \
     security_version_gw.h \
     signature_manager_factory.h \
     signature_manager.h \
+    components/fs_comps_factory.h \
     components/fs_cert_ops.cpp \
     components/fs_cert_ops.h \
-    components/fs_comps_factory.h \
     components/fs_comps_ops.cpp \
     components/fs_comps_ops.h \
     components/fs_synce_ops.cpp \