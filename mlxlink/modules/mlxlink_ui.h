9c35cecccde224d1372f86f3f488b295e8785d75
Harel Karni <hkarni@nvidia.com>
2026-07-16 12:35:03 +0300

    mlxlink | adding support for --show_prr
    Description:
    
    MSTFlint port needed:
    Tested OS:
    Tested devices:
    Tested flows:
    
    Known gaps (with RM ticket):
    
    Issue:

--- mlxlink/modules/mlxlink_ui.h
+++ mlxlink/modules/mlxlink_ui.h
@@ -73,6 +73,7 @@ protected:
     virtual void validatePeriodicEqParams();
     virtual void validateMultiPortInfoParams();
     virtual void validateBkvParams();
+    virtual void validatePrrParams();
     virtual void validatePlrParams();
     virtual void paramValidate();
     virtual void createMlxlinkCommander();
